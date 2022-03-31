f95541d5e722ca751d366e6aa6ecdb13f4700e70
devdave <devdave@ominian.net>
2022-03-30 18:26:12 -0600

    use the nifty derive default to add a ::default func to our struct

--- src/position.rs
+++ src/position.rs
@@ -1,16 +1,5 @@
+#[derive(Default)]
 pub struct Position {
     pub x: usize,
     pub y: usize,
-}
-
-impl Position {
-
-    pub fn default () -> Self {
-        Self {
-            x: 0,
-            y: 0,
-        }
-    }
-
-
 }
