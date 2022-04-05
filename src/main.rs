8627faccd10ac9d3471a817745a90b7744baf629
devdave <devdave@ominian.net>
2022-04-05 10:59:01 -0600

    switch on some specific things for clippy to ignore

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+
+#![allow(clippy::missing_docs_in_private_items, clippy::implicit_return, clippy::shadow_reuse, clippy::print_stdout, clippy::wildcard_enum_match_arm, clippy::else_if_without_else)]
 mod editor;
 mod terminal;
 mod position;
